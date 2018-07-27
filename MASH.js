54044e3c688315b9cc97f8aeab35e80770e7e673
SaifASC5 <saifgyro@gmail.com>
2018-07-27 15:48:39 -0400

    ligma

--- MASH.js
+++ MASH.js
@@ -1,5 +1,22 @@
 function mash(){
-     return "house";
+    return "You will live in a "  + getHome() + ", and you will have " + getChildrenCount() +  "kids!";
 
 }
 console.log(mash());
+
+function getHome(){
+   const places = ["mansion", "apartment", "Shack", "House"];
+   const randomvalue = Math.floor(Math.random() * 4);
+   return places[randomvalue]; 
+}
+
+console.log(getHome());  
+
+function getChildrenCount(){
+    const num = Math.random();
+    const temp100 = num * 100
+    const final = Math.floor(temp100)
+    console.log(final);
+}
+
+getChildrenCount()
